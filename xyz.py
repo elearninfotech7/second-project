ac4921da547f98d23fe69869157eb207ed579d54
elearninfotech7 <104984036+elearninfotech7@users.noreply.github.com>
2022-08-19 18:26:10 +0530

    Example for File upload

---
+++ xyz.py
@@ -0,0 +1,12 @@
+class abc:
+    '''add program starting'''
+    print('welcome')
+
+    def __init__(self):
+        print('example for const')
+
+    def m(self):
+        print('example for method')
+
+f=abc()
+f.m()
